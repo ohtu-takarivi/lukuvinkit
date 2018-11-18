e9be7365a874b227cb06228102c0d9d1dbd7b56c
Sampo Hippeläinen <sampo.hippelainen@gmail.com>
2018-11-18 16:12:05 +0200

    merge CustomUser tests properly

--- src/test/java/ohtu/takarivi/lukuvinkit/CustomUserTest.java
+++ src/test/java/ohtu/takarivi/lukuvinkit/CustomUserTest.java
@@ -52,26 +52,6 @@ public class CustomUserTest {
         cu.setName("name2");
         assertEquals("name2", cu.getName());
     }
-        
-    @Test
-    public void canSetAndGetReadingTips() {
-        List<ReadingTip> readingTips = new ArrayList<>();
-        readingTips.add(new ReadingTip());
-        cu.setReadingTips(readingTips);
-        assertEquals(readingTips, cu.getReadingTips());
-    }
-    
-    @Test
-    public void validateUsername() {
-        assertTrue(CustomUser.isValidUsername("username"));
-        assertTrue(CustomUser.isValidUsername("user_name"));
-        assertTrue(CustomUser.isValidUsername("user1337"));
-        assertFalse(CustomUser.isValidUsername("x"));
-        assertFalse(CustomUser.isValidUsername("an_user_name_that_is_way_too_long_and_reads_more_like_a_story"));
-        assertFalse(CustomUser.isValidUsername("SpecialChars!"));
-        assertFalse(CustomUser.isValidUsername("alpha beta"));
-        assertFalse(CustomUser.isValidUsername(""));
-    }
 
     @Test
     public void canSetReadingTips() {
@@ -85,31 +65,26 @@ public class CustomUserTest {
 
     @Test
     public void validUsernameIsValid() {
-    	CustomUser cu = new CustomUser("user", "password", "name");
-        assertEquals(true, CustomUser.isValidUsername(cu.getUsername()));
+        assertEquals(true, CustomUser.isValidUsername("user"));
     }
 
     @Test
     public void tooShortUsernameIsInvalid() {
-    	CustomUser cu = new CustomUser("u", "password", "name");
-        assertEquals(false, CustomUser.isValidUsername(cu.getUsername()));
+        assertEquals(false, CustomUser.isValidUsername("u"));
     }
 
     @Test
     public void tooLongUsernameIsInvalid() {
-    	CustomUser cu = new CustomUser("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "password", "name");
-        assertEquals(false, CustomUser.isValidUsername(cu.getUsername()));
+        assertEquals(false, CustomUser.isValidUsername("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
     }
 
     @Test
     public void emptyUsernameIsInvalid() {
-    	CustomUser cu = new CustomUser("", "password", "name");
-        assertEquals(false, CustomUser.isValidUsername(cu.getUsername()));
+        assertEquals(false, CustomUser.isValidUsername(""));
     }
 
     @Test
     public void specialCharacterUsernameIsInvalid() {
-    	CustomUser cu = new CustomUser("%%%%%", "password", "name");
-        assertEquals(false, CustomUser.isValidUsername(cu.getUsername()));
+        assertEquals(false, CustomUser.isValidUsername("%%%%%"));
     }
 }
