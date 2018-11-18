31acefc0cc58a900a0c434410d01d30a573ed039
hjoonas <heik.joonas@gmail.com>
2018-11-18 13:00:28 +0200

    Add CustomerUser test cases

--- src/test/java/ohtu/takarivi/lukuvinkit/CustomUserTest.java
+++ src/test/java/ohtu/takarivi/lukuvinkit/CustomUserTest.java
@@ -2,9 +2,15 @@ package ohtu.takarivi.lukuvinkit;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.assertj.core.util.Arrays;
 import org.junit.Test;
 
 import ohtu.takarivi.lukuvinkit.domain.CustomUser;
+
+import ohtu.takarivi.lukuvinkit.domain.ReadingTip;
 
 public class CustomUserTest {
 
@@ -51,5 +57,36 @@ public class CustomUserTest {
 	cu.setName("name2");
 	assertEquals("name2", cu.getName());
     }
-	
+
+    @Test
+    public void canSetReadingTips() {
+    	CustomUser cu = new CustomUser("user", "password", "name");
+        ReadingTip readingTip = new ReadingTip("title", "description", "url", cu);
+        List<ReadingTip> readingTips = new ArrayList<>();
+        readingTips.add(readingTip);
+        cu.setReadingTips(readingTips);
+        assertEquals(readingTip, cu.getReadingTips().get(0));
+    }
+
+    @Test
+    public void canGetReadingTips() {
+    	CustomUser cu = new CustomUser("user", "password", "name");
+        ReadingTip readingTip = new ReadingTip("title", "description", "url", cu);
+        List<ReadingTip> readingTips = new ArrayList<>();
+        readingTips.add(readingTip);
+        cu.setReadingTips(readingTips);
+        assertEquals(readingTip, cu.getReadingTips().get(0));
+    }
+
+    @Test
+    public void validUsernameIsValid() {
+    	CustomUser cu = new CustomUser("user", "password", "name");
+        assertEquals(true, cu.isValidUsername(cu.getUsername()));
+    }
+
+    @Test
+    public void invalidUsernameIsInvalid() {
+    	CustomUser cu = new CustomUser("u", "password", "name");
+        assertEquals(false, cu.isValidUsername(cu.getUsername()));
+    }
 }
