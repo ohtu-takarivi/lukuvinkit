a81a262e461a6fd8d9d9fe4951a612a3398278a3
hjoonas <heik.joonas@gmail.com>
2018-11-18 12:32:04 +0200

    Fix checkstyle

--- src/test/java/ohtu/takarivi/lukuvinkit/ReadingTipTest.java
+++ src/test/java/ohtu/takarivi/lukuvinkit/ReadingTipTest.java
@@ -12,68 +12,68 @@ public class ReadingTipTest {
 
     @Test
     public void constructorNoException() {
-    CustomUser cu = new CustomUser("user", "password", "name");
-	new ReadingTip("title", "description", "url", cu);
+        CustomUser cu = new CustomUser("user", "password", "name");
+        new ReadingTip("title", "description", "url", cu);
     }
 
     @Test
     public void canGetTitle() {
-    CustomUser cu = new CustomUser("user", "password", "name");
-	ReadingTip readingTip = new ReadingTip("title", "description", "url", cu);
-	assertEquals("title", readingTip.getTitle());
+        CustomUser cu = new CustomUser("user", "password", "name");
+        ReadingTip readingTip = new ReadingTip("title", "description", "url", cu);
+        assertEquals("title", readingTip.getTitle());
     }
 
     @Test
     public void canSetTitle() {
-    CustomUser cu = new CustomUser("user", "password", "name");
-    ReadingTip readingTip = new ReadingTip("title", "description", "url", cu);
-    readingTip.setTitle("title2");
-	assertEquals("title2", readingTip.getTitle());
+        CustomUser cu = new CustomUser("user", "password", "name");
+        ReadingTip readingTip = new ReadingTip("title", "description", "url", cu);
+        readingTip.setTitle("title2");
+        assertEquals("title2", readingTip.getTitle());
     }
     
     @Test
     public void canGetDescription() {
-    CustomUser cu = new CustomUser("user", "password", "name");
-	ReadingTip readingTip = new ReadingTip("title", "description", "url", cu);
-	assertEquals("description", readingTip.getDescription());
+        CustomUser cu = new CustomUser("user", "password", "name");
+        ReadingTip readingTip = new ReadingTip("title", "description", "url", cu);
+        assertEquals("description", readingTip.getDescription());
     }
 
     @Test
     public void canSetDescription() {
-    CustomUser cu = new CustomUser("user", "password", "name");
-    ReadingTip readingTip = new ReadingTip("title", "description", "url", cu);
-    readingTip.setDescription("description2");
-	assertEquals("description2", readingTip.getDescription());
+        CustomUser cu = new CustomUser("user", "password", "name");
+        ReadingTip readingTip = new ReadingTip("title", "description", "url", cu);
+        readingTip.setDescription("description2");
+        assertEquals("description2", readingTip.getDescription());
     }
 
     @Test
     public void canGetUrl() {
-    CustomUser cu = new CustomUser("user", "password", "name");
-	ReadingTip readingTip = new ReadingTip("title", "description", "url", cu);
-	assertEquals("url", readingTip.getUrl());
+        CustomUser cu = new CustomUser("user", "password", "name");
+        ReadingTip readingTip = new ReadingTip("title", "description", "url", cu);
+        assertEquals("url", readingTip.getUrl());
     }
 
     @Test
     public void canSetUrl() {
-    CustomUser cu = new CustomUser("user", "password", "name");
-    ReadingTip readingTip = new ReadingTip("title", "description", "url", cu);
-    readingTip.setUrl("url2");
-	assertEquals("url2", readingTip.getUrl());
+        CustomUser cu = new CustomUser("user", "password", "name");
+        ReadingTip readingTip = new ReadingTip("title", "description", "url", cu);
+        readingTip.setUrl("url2");
+        assertEquals("url2", readingTip.getUrl());
     }
 
     @Test
     public void canGetCustomUser() {
-    CustomUser cu = new CustomUser("user", "password", "name");
-	ReadingTip readingTip = new ReadingTip("title", "description", "url", cu);
-	assertEquals("user", readingTip.getCustomUser().getUsername());
+        CustomUser cu = new CustomUser("user", "password", "name");
+        ReadingTip readingTip = new ReadingTip("title", "description", "url", cu);
+        assertEquals("user", readingTip.getCustomUser().getUsername());
     }
 
     @Test
     public void canSetCustomUser() {
-    CustomUser cu = new CustomUser("user", "password", "name");
-    CustomUser cu2 = new CustomUser("user2", "password", "name");
-    ReadingTip readingTip = new ReadingTip("title", "description", "url", cu);
-    readingTip.setCustomUser(cu2);
-	assertEquals("user2", readingTip.getCustomUser().getUsername());
+        CustomUser cu = new CustomUser("user", "password", "name");
+        CustomUser cu2 = new CustomUser("user2", "password", "name");
+        ReadingTip readingTip = new ReadingTip("title", "description", "url", cu);
+        readingTip.setCustomUser(cu2);
+        assertEquals("user2", readingTip.getCustomUser().getUsername());
     }
 }
