0f441044d272e5e2659a8aebb5d66c1f84358b8c
hjoonas <heik.joonas@gmail.com>
2018-11-18 13:13:00 +0200

    Add CustomUser test cases

--- src/test/java/ohtu/takarivi/lukuvinkit/CustomUserTest.java
+++ src/test/java/ohtu/takarivi/lukuvinkit/CustomUserTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.assertj.core.util.Arrays;
 import org.junit.Test;
 
 import ohtu.takarivi.lukuvinkit.domain.CustomUser;
@@ -69,24 +68,32 @@ public class CustomUserTest {
     }
 
     @Test
-    public void canGetReadingTips() {
-    	CustomUser cu = new CustomUser("user", "password", "name");
-        ReadingTip readingTip = new ReadingTip("title", "description", "url", cu);
-        List<ReadingTip> readingTips = new ArrayList<>();
-        readingTips.add(readingTip);
-        cu.setReadingTips(readingTips);
-        assertEquals(readingTip, cu.getReadingTips().get(0));
-    }
-
-    @Test
     public void validUsernameIsValid() {
     	CustomUser cu = new CustomUser("user", "password", "name");
-        assertEquals(true, cu.isValidUsername(cu.getUsername()));
+        assertEquals(true, CustomUser.isValidUsername(cu.getUsername()));
     }
 
     @Test
-    public void invalidUsernameIsInvalid() {
+    public void tooShortUsernameIsInvalid() {
     	CustomUser cu = new CustomUser("u", "password", "name");
-        assertEquals(false, cu.isValidUsername(cu.getUsername()));
+        assertEquals(false, CustomUser.isValidUsername(cu.getUsername()));
+    }
+
+    @Test
+    public void tooLongUsernameIsInvalid() {
+    	CustomUser cu = new CustomUser("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "password", "name");
+        assertEquals(false, CustomUser.isValidUsername(cu.getUsername()));
+    }
+
+    @Test
+    public void emptyUsernameIsInvalid() {
+    	CustomUser cu = new CustomUser("", "password", "name");
+        assertEquals(false, CustomUser.isValidUsername(cu.getUsername()));
+    }
+
+    @Test
+    public void specialCharacterUsernameIsInvalid() {
+    	CustomUser cu = new CustomUser("%%%%%", "password", "name");
+        assertEquals(false, CustomUser.isValidUsername(cu.getUsername()));
     }
 }
