d514c06d5e101bbee4eefbd94b82e954e1c73f1e
thalvari <tuomas.halvari@helsinki.fi>
2018-11-19 20:23:51 +0200

    removed excess tests because of lombok

--- src/test/java/ohtu/takarivi/lukuvinkit/CustomUserTest.java
+++ src/test/java/ohtu/takarivi/lukuvinkit/CustomUserTest.java
@@ -1,68 +1,19 @@
 package ohtu.takarivi.lukuvinkit;
 
-import static org.junit.Assert.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import ohtu.takarivi.lukuvinkit.domain.CustomUser;
 import org.junit.Before;
 import org.junit.Test;
 
-import ohtu.takarivi.lukuvinkit.domain.CustomUser;
-import ohtu.takarivi.lukuvinkit.domain.ReadingTip;
-import org.junit.Ignore;
-
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class CustomUserTest {
-    
+
     CustomUser cu;
 
     @Before
     public void before() {
         cu = new CustomUser("user", "password", "name");
-    }
-
-    @Test
-    public void canGetUsername() {
-        assertEquals("user", cu.getUsername());
-    }
-
-    @Test
-    public void canGetPassword() {
-        assertEquals("password", cu.getPassword());
-    }
-
-    @Test
-    public void canGetName() {
-        assertEquals("name", cu.getName());
-    }
-
-    @Test
-    public void canSetUsername() {
-        cu.setUsername("user2");
-        assertEquals("user2", cu.getUsername());
-    }
-
-    @Test
-    public void canSetPassword() {
-        cu.setPassword("password2");
-        assertEquals("password2", cu.getPassword());
-    }
-
-    @Test
-    public void canSetName() {
-        cu.setName("name2");
-        assertEquals("name2", cu.getName());
-    }
-
-    @Test
-    public void canSetReadingTips() {
-        CustomUser cu = new CustomUser("user", "password", "name");
-        ReadingTip readingTip = new ReadingTip("title", "description", "url", cu);
-        List<ReadingTip> readingTips = new ArrayList<>();
-        readingTips.add(readingTip);
-        cu.setReadingTips(readingTips);
-        assertEquals(readingTip, cu.getReadingTips().get(0));
     }
 
     @Test
@@ -89,4 +40,5 @@ public class CustomUserTest {
     public void specialCharacterUsernameIsInvalid() {
         assertFalse(CustomUser.isValidUsername("%%%%%"));
     }
+
 }
--- src/test/java/ohtu/takarivi/lukuvinkit/ReadingTipTest.java
+++ src/test/java/ohtu/takarivi/lukuvinkit/ReadingTipTest.java
@@ -1,13 +1,8 @@
 package ohtu.takarivi.lukuvinkit;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
 import ohtu.takarivi.lukuvinkit.domain.CustomUser;
-
 import ohtu.takarivi.lukuvinkit.domain.ReadingTip;
-import org.junit.Ignore;
+import org.junit.Test;
 
 
 public class ReadingTipTest {
@@ -18,64 +13,4 @@ public class ReadingTipTest {
         new ReadingTip("title", "description", "url", cu);
     }
 
-    @Test
-    public void canGetTitle() {
-        CustomUser cu = new CustomUser("user", "password", "name");
-        ReadingTip readingTip = new ReadingTip("title", "description", "url", cu);
-        assertEquals("title", readingTip.getTitle());
-    }
-
-    @Test
-    public void canSetTitle() {
-        CustomUser cu = new CustomUser("user", "password", "name");
-        ReadingTip readingTip = new ReadingTip("title", "description", "url", cu);
-        readingTip.setTitle("title2");
-        assertEquals("title2", readingTip.getTitle());
-    }
-    
-    @Test
-    public void canGetDescription() {
-        CustomUser cu = new CustomUser("user", "password", "name");
-        ReadingTip readingTip = new ReadingTip("title", "description", "url", cu);
-        assertEquals("description", readingTip.getDescription());
-    }
-
-    @Test
-    public void canSetDescription() {
-        CustomUser cu = new CustomUser("user", "password", "name");
-        ReadingTip readingTip = new ReadingTip("title", "description", "url", cu);
-        readingTip.setDescription("description2");
-        assertEquals("description2", readingTip.getDescription());
-    }
-
-    @Test
-    public void canGetUrl() {
-        CustomUser cu = new CustomUser("user", "password", "name");
-        ReadingTip readingTip = new ReadingTip("title", "description", "url", cu);
-        assertEquals("url", readingTip.getUrl());
-    }
-
-    @Test
-    public void canSetUrl() {
-        CustomUser cu = new CustomUser("user", "password", "name");
-        ReadingTip readingTip = new ReadingTip("title", "description", "url", cu);
-        readingTip.setUrl("url2");
-        assertEquals("url2", readingTip.getUrl());
-    }
-
-    @Test
-    public void canGetCustomUser() {
-        CustomUser cu = new CustomUser("user", "password", "name");
-        ReadingTip readingTip = new ReadingTip("title", "description", "url", cu);
-        assertEquals("user", readingTip.getCustomUser().getUsername());
-    }
-
-    @Test
-    public void canSetCustomUser() {
-        CustomUser cu = new CustomUser("user", "password", "name");
-        CustomUser cu2 = new CustomUser("user2", "password", "name");
-        ReadingTip readingTip = new ReadingTip("title", "description", "url", cu);
-        readingTip.setCustomUser(cu2);
-        assertEquals("user2", readingTip.getCustomUser().getUsername());
-    }
 }
