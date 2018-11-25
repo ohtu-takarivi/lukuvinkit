2c9a25a7fe1703d929913ca3d225ad46fbfcdcd5
Sampo Hippeläinen <sampo.hippelainen@gmail.com>
2018-11-26 01:21:40 +0200

    increase max name and password length; esp. the latter has no reason to be this low

--- src/main/java/ohtu/takarivi/lukuvinkit/forms/CustomUserRegisterForm.java
+++ src/main/java/ohtu/takarivi/lukuvinkit/forms/CustomUserRegisterForm.java
@@ -14,11 +14,11 @@ import javax.validation.constraints.Size;
 public class CustomUserRegisterForm {
 
     private static final int NAME_MIN_LENGTH = 2;
-    private static final int NAME_MAX_LENGTH = 32;
+    private static final int NAME_MAX_LENGTH = 64;
     private static final int USERNAME_MIN_LENGTH = 3;
     private static final int USERNAME_MAX_LENGTH = 32;
     private static final int PASSWORD_MIN_LENGTH = 8;
-    private static final int PASSWORD_MAX_LENGTH = 32;
+    private static final int PASSWORD_MAX_LENGTH = 256;
 
     @NotEmpty
     @Size(min = NAME_MIN_LENGTH, max = NAME_MAX_LENGTH, message = "Nimen pituus 2-32 merkkiä")
