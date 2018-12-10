a9d2255e9dde4f634c6631a6e4dfa94ae112f433
thalvari <tuomas.halvari@helsinki.fi>
2018-12-10 22:30:33 +0200

    fixed testDataInsert

--- src/main/java/ohtu/takarivi/lukuvinkit/controller/MiscController.java
+++ src/main/java/ohtu/takarivi/lukuvinkit/controller/MiscController.java
@@ -1,17 +1,5 @@
 package ohtu.takarivi.lukuvinkit.controller;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
 import ohtu.takarivi.lukuvinkit.domain.CustomUser;
 import ohtu.takarivi.lukuvinkit.domain.ReadingTip;
 import ohtu.takarivi.lukuvinkit.domain.ReadingTipCategory;
@@ -20,6 +8,17 @@ import ohtu.takarivi.lukuvinkit.forms.FormUtils;
 import ohtu.takarivi.lukuvinkit.repository.CustomUserRepository;
 import ohtu.takarivi.lukuvinkit.repository.ReadingTipRepository;
 import ohtu.takarivi.lukuvinkit.repository.ReadingTipTagRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Set;
 
 /**
  * The Spring controller for miscellaneous pages.
@@ -43,7 +42,7 @@ public class MiscController {
 
     @Autowired
     private PasswordEncoder encoder;
-    
+
     /**
      * Enables the test data entry form; this is only used by testing code.
      */
@@ -55,7 +54,6 @@ public class MiscController {
      * The page that fetches the title and description of a remote URL.
      *
      * @param auth An Authentication object representing the currently authenticated user.
-     * @param url The URL to get the information from; this is a GET parameter.
      * @return The JSON text returned by this API endpoint.
      */
     @GetMapping("/testDataInsert")
@@ -63,43 +61,34 @@ public class MiscController {
         if (!ENABLE_TESTDATA_ENTRY) {
             return "redirect:/";
         }
-        
         readingTipRepository.deleteAll();
         customUserRepository.deleteAll();
-        
         CustomUser user = new CustomUser("testuser", encoder.encode("testuser"), "testuser");
         customUserRepository.save(user);
-        
-        File file = new File(getClass().getClassLoader().getResource("testdata.txt").getFile());
-        
-        try (Scanner scanner = new Scanner(file, "utf-8")) {
-            for (;;) {
-                ReadingTip rtip = getNextReadingTipFrom(scanner, user);
+        InputStream is = getClass().getResourceAsStream("/testdata.txt");
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+            while (true) {
+                ReadingTip rtip = getNextReadingTipFrom(br, user);
                 if (rtip == null) {
                     break;
                 }
                 readingTipRepository.save(rtip);
             }
-
-            scanner.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
         auth.setAuthenticated(false);
-
         return "redirect:/login#user_and_password_is_testuser";
     }
 
     /**
      * Reads the next reading tip from a Scanner providing test data.
-     * 
-     * @param scanner The Scanner to read from.
-     * @param emptyTags The Set of ReadingTipTag objects to be provided as the tags.
+     *
+     * @param br         The Scanner to read from.
      * @param customUser The CustomUser to be passed to the ReadingTip constructor.
      * @return The next ReadingTip or null if there is no more (valid) test data.
      */
-    private ReadingTip getNextReadingTipFrom(Scanner scanner, CustomUser customUser) {
+    private ReadingTip getNextReadingTipFrom(BufferedReader br, CustomUser customUser) throws IOException {
         // testdata format:
         /// category (BOOK, LINK, VIDEO, ARTICLE)
         /// title
@@ -107,40 +96,35 @@ public class MiscController {
         /// description
         /// tags
         /// URL if VIDEO or ARTICLE, ISBN if BOOK, otherwise this line does not exist
-        
-        if (!scanner.hasNextLine()) {
+        String cattext = br.readLine();
+        // allow empty lines between test data entries
+        while (cattext != null && cattext.trim().isEmpty()) {
+            cattext = br.readLine();
+        }
+        if (cattext == null) {
             return null;
         }
-        
-        // allow empty lines between test data entries
-        String cattext = scanner.nextLine();
-        while (scanner.hasNextLine() && cattext.trim().isEmpty()) {
-            cattext = scanner.nextLine();
-        }
-        
         ReadingTipCategory category = ReadingTipCategory.getByName(cattext);
         // stop reading if the category is invalid
         if (category == null) {
             return null;
         }
-        
-        String title = scanner.nextLine();
-        String author = scanner.nextLine();
-        String description = scanner.nextLine();
-        String tagString = scanner.nextLine();
+        String title = br.readLine();
+        String author = br.readLine();
+        String description = br.readLine();
+        String tagString = br.readLine();
         String url = "";
         String isbn = "";
-        
         // read URL only if applicable
         if (category == ReadingTipCategory.LINK || category == ReadingTipCategory.VIDEO) {
-            url = scanner.nextLine();
+            url = br.readLine();
         }
         // read ISBN only if applicable
         if (category == ReadingTipCategory.BOOK) {
-            isbn = scanner.nextLine();
+            isbn = br.readLine();
         }
-        
         Set<ReadingTipTag> tags = FormUtils.prepareTags(readingTipTagRepository, tagString.split(" "));
         return new ReadingTip(title, category, description, url, author, isbn, tags, customUser);
     }
+
 }
